20de194dff07805b5d76d86e86181a03091079d7
pasindufe <pasinduf@fortude.co>
2023-06-28 13:06:17 +0530

    add env-cmd

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Welcome test react app build with jenkins-ec2</p>
+
+        <p>env is {process.env.REACT_APP_ENV}</p>
+        <p>value : {process.env.REACT_APP_KEY1}</p>
       </header>
     </div>
   );
